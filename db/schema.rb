9e03ddc4e317bf8a6c703efec78365573516b2cc
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-24 08:37:38 +0900

    [add]dbにgear_powersテーブルを新規作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_15_150538) do
+ActiveRecord::Schema.define(version: 2022_10_23_233303) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,18 @@ ActiveRecord::Schema.define(version: 2022_10_15_150538) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "gear_powers", force: :cascade do |t|
+    t.string "name"
+    t.string "effect"
+    t.string "easy_brand1"
+    t.string "easy_brand2"
+    t.string "hard_brand1"
+    t.string "hard_brand2"
+    t.string "main_part"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
