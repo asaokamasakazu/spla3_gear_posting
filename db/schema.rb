09b26287b45e616c432fa8125d54fa234626883c
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-24 08:51:06 +0900

    [update]dbのgear_powersテーブルにabbreviationカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_23_233303) do
+ActiveRecord::Schema.define(version: 2022_10_23_234915) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_10_23_233303) do
     t.string "main_part"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "abbreviation"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
