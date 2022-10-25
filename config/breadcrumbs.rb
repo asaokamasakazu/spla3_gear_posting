a60c29cba050c65250700719a1dd667a772bfa4c
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-25 11:09:33 +0900

    [add]ギアパワーの個別ページのパンくず設定を追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,3 +6,8 @@ crumb :gear_powers_index do
   link "ギアパワー", gear_powers_path
   parent :root
 end
+
+crumb :gear_powers_show do |gear_power|
+  link gear_power.name, gear_power_path(gear_power)
+  parent :gear_powers_index
+end
