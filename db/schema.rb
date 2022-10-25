37f6697b301a14e9e087bc9dfa8ebf3604ff39dc
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-25 10:47:28 +0900

    [add]gear_powersテーブルのnameカラムとeffectカラムに対してunique indexを追加
    バリデーションの設定しかしていなかったため後からindexを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_23_234915) do
+ActiveRecord::Schema.define(version: 2022_10_25_012656) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2022_10_23_234915) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "abbreviation"
+    t.index ["effect"], name: "index_gear_powers_on_effect", unique: true
+    t.index ["name"], name: "index_gear_powers_on_name", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
