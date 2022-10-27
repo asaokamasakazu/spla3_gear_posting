698573a97a59d5d4fda3196173abbe4bf101cd78
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-27 09:03:10 +0900

    [add]gear_powerの一覧ページのリクエストスペックを作成

--- spec/requests/gear_powers_spec.rb
+++ spec/requests/gear_powers_spec.rb
@@ -2,17 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "GearPowers", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/gear_powers/index"
+    let!(:gear_power1) { create(:gear_power1) }
+    let!(:gear_power2) { create(:gear_power2) }
+
+    before do
+      get gear_powers_path
+    end
+
+    it "正常なレスポンスを返すこと" do
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/gear_powers/show"
-      expect(response).to have_http_status(:success)
+    it "ギアパワーを全件取得していること" do
+      expect(response.body).to include gear_power1.name
+      expect(response.body).to include gear_power2.name
+    end
+
+    it "ギアパワーの各情報を取得していること" do
+      expect(response.body).to include gear_power1.effect
+      expect(response.body).to include "・" + gear_power1.easy_brand1
+      expect(response.body).to include "・" + gear_power1.easy_brand2
+      expect(response.body).to include "・" + gear_power1.hard_brand1
+      expect(response.body).to include "・" + gear_power1.hard_brand2
+      expect(response.body).to include gear_power1.main_part
+      expect(response.body).to include gear_power1.abbreviation
+      expect(response.body).to include "なし"
+    end
+
+    it "ギアパワーのアイコン画像を取得していること" do
+      expect(response.body).to include "gear_power_#{gear_power1.id}.png"
     end
   end
-
 end
