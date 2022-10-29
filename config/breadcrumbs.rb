0db43aa2070c46d27150dd09f5f31aafc8bba622
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-29 18:10:11 +0900

    [add]/users/newのパンくずを設定

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -11,3 +11,8 @@ crumb :gear_powers_show do |gear_power|
   link gear_power.name, gear_power_path(gear_power)
   parent :gear_powers_index
 end
+
+crumb :users_new do
+  link "ユーザー登録", new_user_registration_path
+  parent :root
+end
