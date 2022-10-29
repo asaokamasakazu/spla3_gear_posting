f81946c0e9d775e8b1b08c69cfe7d42bce122915
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-29 18:09:40 +0900

    [add]/usersへのアクセスで/users/newが表示されるように設定
    users/newにてバリデーションエラー後の画面でリロードを行うとルーティングエラーとなるため設定しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users' => 'users/registrations#new'
+  end
   resources :gear_powers, only: [:index, :show]
   root :to => 'home#top'
 end
