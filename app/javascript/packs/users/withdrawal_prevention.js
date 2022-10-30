a1e74396f41d4a2a18d0378831f463d9f506aed7
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-30 09:13:30 +0900

    [update]消し忘れていたコメント削除

--- app/javascript/packs/users/withdrawal_prevention.js
+++ app/javascript/packs/users/withdrawal_prevention.js
@@ -1,16 +1,11 @@
-// beforeunloadイベント発火時の処理
 var unloaded = function (e) {
-  // EdgeとIEでのみ表示されるカスタムメッセージ
   var confirmMessage = "入力内容が破棄されます。";
   e.returnValue = confirmMessage;
   return confirmMessage;
 };
 
-// beforeunloadイベントの登録
 window.addEventListener("beforeunload", unloaded, false);
 
-// 特定のボタンがクリックされたときはアラートを表示しない
 document.getElementById("beforeunload-off").addEventListener("click", function(){
-  // submit時はアラート表示しない
   window.removeEventListener("beforeunload", unloaded, false);
 });
