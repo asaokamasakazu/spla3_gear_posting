f850cee66b396b77597e609b636536f51349efb6
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-01 00:12:10 +0900

    [add]ユーザーのアカウントページのアクションを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, {only: [:account]}
+
   def list
     @users = User.all.order(created_at: :desc)
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def account
+    @user = current_user
   end
 end
