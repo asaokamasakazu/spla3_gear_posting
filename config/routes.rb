c1f2e9c068fc7aed17da071ab8dbe9b41b994271
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-31 10:00:04 +0900

    [add]ユーザー詳細ページのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users' => 'users/registrations#new'
   end
+  resources :users, only: [:show]
   resources :gear_powers, only: [:index, :show]
   root :to => 'home#top'
 end
