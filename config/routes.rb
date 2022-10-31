885ef9a7442994a487f695e3689a41d243e0c2c3
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-31 19:46:25 +0900

    [add]ユーザーのアカウントページへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     collection do
       get 'list'
+      get 'account'
     end
   end
   resources :gear_powers, only: [:index, :show]
