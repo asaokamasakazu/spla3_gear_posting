1c627ea55196d15a95267b958af13165a09f1918
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-01 17:43:39 +0900

    [add]アカウント編集ページのストロングパラメーターにimageを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:rank])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:prowess])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :rank, :prowess, :profile, :image])
   end
 end
