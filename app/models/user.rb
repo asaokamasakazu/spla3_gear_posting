5c72aea7e53c66968a5cd730e1ddf2dc230044c0
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-01 17:49:10 +0900

    [add]edit時に現在のパスワードが不要になるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,17 @@ class User < ApplicationRecord
   validates :profile, { length: { maximum: 100 } }
 
   has_one_attached :image
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank? 
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
