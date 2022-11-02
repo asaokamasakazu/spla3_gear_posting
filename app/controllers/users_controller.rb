14c45c70221e1de304625eddbb9f8c81a87a13db
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-02 16:48:33 +0900

    [add]ユーザーの検索結果ページのアクションを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, { only: [:account] }
+  before_action :set_q, { only: [:search, :list] }
 
   def list
     @users = User.all.order(created_at: :desc)
@@ -11,5 +12,15 @@ class UsersController < ApplicationController
 
   def account
     @user = current_user
+  end
+
+  def search
+    @results = @q.result.order(created_at: :desc)
+  end
+
+  private
+
+  def set_q
+    @q = User.ransack(params[:q])
   end
 end
