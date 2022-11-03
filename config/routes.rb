d4c577e034a5215f7ba75dc62627a6882fadd7d2
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-03 10:59:32 +0900

    [add]ゲストログイン用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+  devise_scope :user do
+    post "users/guest_sign_in" => "users/sessions#guest_sign_in"
+  end
   resources :users, only: [:show] do
     collection do
       get "list"
