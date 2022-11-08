2183758074bc18ce115313b51cf3d153a9cbd569
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-08 16:10:10 +0900

    [add]ラジオボタン下のメッセージが動的に消えるjsを作成

---
+++ app/javascript/packs/posts/required_radio_message_off.js
@@ -0,0 +1,73 @@
+$(function () {
+  let radio_head_main = document.getElementById("radio-head-main");
+  let message_head_main = document.getElementById("message-head-main");
+  $(radio_head_main).on('click', () => {
+      $(message_head_main).hide();
+  });
+
+  let radio_head_sub1 = document.getElementById("radio-head-sub1");
+  let message_head_sub1 = document.getElementById("message-head-sub1");
+  $(radio_head_sub1).on('click', () => {
+      $(message_head_sub1).hide();
+  });
+
+  let radio_head_sub2 = document.getElementById("radio-head-sub2");
+  let message_head_sub2 = document.getElementById("message-head-sub2");
+  $(radio_head_sub2).on('click', () => {
+      $(message_head_sub2).hide();
+  });
+
+  let radio_head_sub3 = document.getElementById("radio-head-sub3");
+  let message_head_sub3 = document.getElementById("message-head-sub3");
+  $(radio_head_sub3).on('click', () => {
+      $(message_head_sub3).hide();
+  });
+
+  let radio_body_main = document.getElementById("radio-body-main");
+  let message_body_main = document.getElementById("message-body-main");
+  $(radio_body_main).on('click', () => {
+      $(message_body_main).hide();
+  });
+
+  let radio_body_sub1 = document.getElementById("radio-body-sub1");
+  let message_body_sub1 = document.getElementById("message-body-sub1");
+  $(radio_body_sub1).on('click', () => {
+      $(message_body_sub1).hide();
+  });
+
+  let radio_body_sub2 = document.getElementById("radio-body-sub2");
+  let message_body_sub2 = document.getElementById("message-body-sub2");
+  $(radio_body_sub2).on('click', () => {
+      $(message_body_sub2).hide();
+  });
+
+  let radio_body_sub3 = document.getElementById("radio-body-sub3");
+  let message_body_sub3 = document.getElementById("message-body-sub3");
+  $(radio_body_sub3).on('click', () => {
+      $(message_body_sub3).hide();
+  });
+
+  let radio_shoes_main = document.getElementById("radio-shoes-main");
+  let message_shoes_main = document.getElementById("message-shoes-main");
+  $(radio_shoes_main).on('click', () => {
+      $(message_shoes_main).hide();
+  });
+
+  let radio_shoes_sub1 = document.getElementById("radio-shoes-sub1");
+  let message_shoes_sub1 = document.getElementById("message-shoes-sub1");
+  $(radio_shoes_sub1).on('click', () => {
+      $(message_shoes_sub1).hide();
+  });
+
+  let radio_shoes_sub2 = document.getElementById("radio-shoes-sub2");
+  let message_shoes_sub2 = document.getElementById("message-shoes-sub2");
+  $(radio_shoes_sub2).on('click', () => {
+      $(message_shoes_sub2).hide();
+  });
+
+  let radio_shoes_sub3 = document.getElementById("radio-shoes-sub3");
+  let message_shoes_sub3 = document.getElementById("message-shoes-sub3");
+  $(radio_shoes_sub3).on('click', () => {
+      $(message_shoes_sub3).hide();
+  });
+});
