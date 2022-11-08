2e9bc7cff0f5b9c7926577030e6dcd5faa64a40d
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-08 15:51:41 +0900

    [add]バリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, { presence: true, length: { maximum: 20 } }
+  validates :weapon, { presence: true }
+  validates :battle, { presence: true }
+  validates :comment, { length: { maximum: 250 } }
+  validates :head_main, { presence: true }
+  validates :head_sub1, { presence: true }
+  validates :head_sub2, { presence: true }
+  validates :head_sub3, { presence: true }
+  validates :body_main, { presence: true }
+  validates :body_sub1, { presence: true }
+  validates :body_sub2, { presence: true }
+  validates :body_sub3, { presence: true }
+  validates :shoes_main, { presence: true }
+  validates :shoes_sub1, { presence: true }
+  validates :shoes_sub2, { presence: true }
+  validates :shoes_sub3, { presence: true }
+
   belongs_to :user
 end
