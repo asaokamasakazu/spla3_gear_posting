29b4416eb33b1df4ae16c9e53653b6ba913846e3
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-09 20:55:01 +0900

    [add]showアクションに@posts_countを追加
    投稿数をカウントしてユーザー詳細ページに記載するために追加しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = Post.where(user_id: @user.id).order(created_at: :desc)
+    @posts_count = @posts.count
   end
 
   def account
