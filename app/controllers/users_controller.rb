8999813f4a89085eb16f17075df0cd264c9984f0
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-10 09:30:27 +0900

    [update]@postsと@posts_countを削除
    @user.postsと@user.posts.countで代用できるため削除しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: @user.id).order(created_at: :desc)
-    @posts_count = @posts.count
   end
 
   def account
