1973cbc60d040626b79a929c88a79689b7727d7f
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-10 11:14:19 +0900

    [add]indexに対するrequest specを作成

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,4 +10,61 @@ RSpec.describe "Posts", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET /index" do
+    let!(:post1) { create(:post, weapon: "わかばシューター", battle: "ガチヤグラ") }
+    let!(:post2) { create(:post, weapon: "スプラシューター", battle: "ナワバリバトル") }
+
+    before do
+      get posts_path
+    end
+
+    it "正常なレスポンスを返すこと" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "投稿の各情報を取得していること" do
+      expect(response.body).to include post1.user.name
+      expect(response.body).to include post1.created_at.to_s(:custom_no_year)
+      expect(response.body).to include post1.title
+      expect(response.body).to include post1.weapon
+      expect(response.body).to include post1.battle
+      expect(response.body).to include post2.user.name
+      expect(response.body).to include post2.created_at.to_s(:custom_no_year)
+      expect(response.body).to include post2.title
+      expect(response.body).to include post2.weapon
+      expect(response.body).to include post2.battle
+    end
+
+    it "ユーザーのアイコン画像を取得していること" do
+      expect(response.body).to include "user_image_default_bk.png"
+    end
+
+    it "ギアパワーの各アイコン画像を取得していること" do
+      expect(response.body).to include "gear_power_#{post1.head_main}.png"
+      expect(response.body).to include "gear_power_#{post1.head_sub1}.png"
+      expect(response.body).to include "gear_power_#{post1.head_sub2}.png"
+      expect(response.body).to include "gear_power_#{post1.head_sub3}.png"
+      expect(response.body).to include "gear_power_#{post1.body_main}.png"
+      expect(response.body).to include "gear_power_#{post1.body_sub1}.png"
+      expect(response.body).to include "gear_power_#{post1.body_sub2}.png"
+      expect(response.body).to include "gear_power_#{post1.body_sub3}.png"
+      expect(response.body).to include "gear_power_#{post1.shoes_main}.png"
+      expect(response.body).to include "gear_power_#{post1.shoes_sub1}.png"
+      expect(response.body).to include "gear_power_#{post1.shoes_sub2}.png"
+      expect(response.body).to include "gear_power_#{post1.shoes_sub3}.png"
+      expect(response.body).to include "gear_power_#{post2.head_main}.png"
+      expect(response.body).to include "gear_power_#{post2.head_sub1}.png"
+      expect(response.body).to include "gear_power_#{post2.head_sub2}.png"
+      expect(response.body).to include "gear_power_#{post2.head_sub3}.png"
+      expect(response.body).to include "gear_power_#{post2.body_main}.png"
+      expect(response.body).to include "gear_power_#{post2.body_sub1}.png"
+      expect(response.body).to include "gear_power_#{post2.body_sub2}.png"
+      expect(response.body).to include "gear_power_#{post2.body_sub3}.png"
+      expect(response.body).to include "gear_power_#{post2.shoes_main}.png"
+      expect(response.body).to include "gear_power_#{post2.shoes_sub1}.png"
+      expect(response.body).to include "gear_power_#{post2.shoes_sub2}.png"
+      expect(response.body).to include "gear_power_#{post2.shoes_sub3}.png"
+    end
+  end
 end
