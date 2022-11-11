844ae85effa77ac653fd895c5598870bbeaf74c7
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-11 16:04:52 +0900

    [update]nameとeffectにfakerを採用
    create_listでまとめて作成できるようにするために変更しました。

--- spec/factories/gear_powers.rb
+++ spec/factories/gear_powers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :gear_power1, class: "GearPower" do
-    name { "ラストスパート" }
-    effect { "対戦終了の30カウント前からインク効率とインク回復速度がかなりアップします。" }
+    name { Faker::Lorem.characters(number: 10) }
+    effect { Faker::Lorem.characters(number: 20) }
     easy_brand1 { "バトロイカ" }
     easy_brand2 { "クラーゲス" }
     hard_brand1 { "エンペリー" }
@@ -10,8 +10,6 @@ FactoryBot.define do
     abbreviation { "ラスパ" }
 
     factory :gear_power2 do
-      name { "相手インク影響軽減" }
-      effect { "相手のインクをふんだ時のダメージや移動速度の減少量が少なくなります。" }
       easy_brand1 { "" }
       easy_brand2 { "" }
       hard_brand1 { "" }
