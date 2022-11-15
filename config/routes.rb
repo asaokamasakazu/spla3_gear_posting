6f3da62404509a3224798591644556052027c525
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-15 10:31:13 +0900

    [add]投稿検索ページのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    collection do
+      get "search"
+    end
+  end
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
