6251973bf274742ad2a8c1525fd67a32d8afffff
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-17 11:02:52 +0900

    [update]redirect_toを削除し、@postを追加
    ajax通信を行うためです。

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,13 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @post = Post.find(params[:post_id])
     Favorite.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_back fallback_location: root_path
   end
 
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
-    @favorite.destroy
-    redirect_back fallback_location: root_path
+    @post = Post.find(params[:post_id])
+    favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
+    favorite.destroy
   end
 end
