3066b8681fee1e845dabf64aabd299c35f279834
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-17 23:45:00 +0900

    [update]被お気に入り数の箇所の記述を調整
    1行短縮するために変更しました。

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user
 
   def create
     @post = Post.find(params[:post_id])
@@ -10,5 +10,13 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
     favorite.destroy
+  end
+
+  # ajax通信ではdeviseのauthenticate_user!が使えないため独自に定義
+  def authenticate_user
+    if current_user.nil?
+      flash[:alert] = "アカウント登録もしくはログインしてください。"
+      redirect_to new_user_session_path
+    end
   end
 end
