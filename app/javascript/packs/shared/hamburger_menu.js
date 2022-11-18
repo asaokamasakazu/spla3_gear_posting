738487d33193611aec10eaa7bd91c190b4b5e197
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-18 20:26:09 +0900

    [update]ヘッダーをレスポンシブ化

--- app/javascript/packs/shared/hamburger_menu.js
+++ app/javascript/packs/shared/hamburger_menu.js
@@ -1,3 +1,7 @@
 $('.nav_toggle').on('click', function () {
   $('.nav_toggle, .nav').toggleClass('show');
 });
+
+$('.nav_toggle_sp').on('click', function () {
+  $('.nav_toggle_sp, .nav_sp').toggleClass('show');
+});
