1da30865e508a4b89a93b213d7d11f37707dc302
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-19 12:41:03 +0900

    [add]@popular_postsを追加し、@postを@latest_postへと変数名を変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def top
-    @posts = Post.all.order(created_at: :desc)
+    @latest_posts = Post.all.order(created_at: :desc)
+    @popular_posts = Post.order(created_at: :desc).includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
     @users = User.all.order(created_at: :desc)
     @gear_powers = GearPower.all
   end
