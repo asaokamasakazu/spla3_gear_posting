1e08c2f8e2df5abc30f710f8396483bdeb4fd4fd
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-19 10:59:40 +0900

    [add]仮置きしていた新着投稿部分を本実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def top
+    @posts = Post.all.order(created_at: :desc)
     @users = User.all.order(created_at: :desc)
     @gear_powers = GearPower.all
   end
