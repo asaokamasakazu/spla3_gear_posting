ad5d293ba7a3f16215174ca093d93ffb275883f6
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-19 13:14:57 +0900

    [update]1行あたりの文字数が100文字を超えていたため途中に改行を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def top
     @latest_posts = Post.all.order(created_at: :desc)
-    @popular_posts = Post.order(created_at: :desc).includes(:favorited_users).sort { |a, b| b.favorited_users.size <=> a.favorited_users.size }
+    @popular_posts = Post.order(created_at: :desc).includes(:favorited_users).
+      sort { |a, b| b.favorited_users.size <=> a.favorited_users.size }
     @users = User.all.order(created_at: :desc)
     @gear_powers = GearPower.all
   end
