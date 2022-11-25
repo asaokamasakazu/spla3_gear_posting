5948add6181121c5f251ff1bc9af63c3b325e0ba
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-11-25 15:09:13 +0900

    [fix]投稿詳細ページのお気に入りしたユーザー部分でN+1問題が発生していたため修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,11 +3,15 @@ class FavoritesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    @post_favorites = @post.favorites.includes(user: { image_attachment: :blob }).
+      order(created_at: :desc)
     Favorite.create(user_id: current_user.id, post_id: params[:post_id])
   end
 
   def destroy
     @post = Post.find(params[:post_id])
+    @post_favorites = @post.favorites.includes(user: { image_attachment: :blob }).
+      order(created_at: :desc)
     favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
     favorite.destroy
   end
