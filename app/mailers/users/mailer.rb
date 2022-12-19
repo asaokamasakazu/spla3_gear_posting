c772d97dd69b07b25eee38abcd1ea7c7a8656c5f
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-12-19 10:37:21 +0900

    [update]イコールの前後に空白を追加

--- app/mailers/users/mailer.rb
+++ app/mailers/users/mailer.rb
@@ -2,7 +2,7 @@ class Users::Mailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
-  def confirmation_instructions(record, token, opts={})
+  def confirmation_instructions(record, token, opts = {})
     if record.unconfirmed_email != nil
       opts[:subject] = "メールアドレス変更手続きを完了してください"
     else
