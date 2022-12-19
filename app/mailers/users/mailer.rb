9b62475f730733bd72a67dce84de1a27f35b8bab
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-12-19 10:38:12 +0900

    [update].nil?メソッドを用いた記述に変更

--- app/mailers/users/mailer.rb
+++ app/mailers/users/mailer.rb
@@ -3,7 +3,7 @@ class Users::Mailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
   def confirmation_instructions(record, token, opts = {})
-    if record.unconfirmed_email != nil
+    if !record.unconfirmed_email.nil?
       opts[:subject] = "メールアドレス変更手続きを完了してください"
     else
       opts[:subject] = "認証を行ってユーザ登録を完了してください"
